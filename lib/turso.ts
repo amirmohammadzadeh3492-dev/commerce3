622e19d8c641b8b439357a80e413a2686c0e985b
amirmohammadzadeh3492-dev <amirmohammadzadeh3492@gmail.com>
2026-07-04 14:52:06 +0330

    Update turso.ts

--- lib/turso.ts
+++ lib/turso.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@libsql/client";
 
 export const turso = createClient({
-  url: process.env.TURSO_DATABASE_URL!,
+  url:libsql://commerce-db-amirmohammadzadeh3492-dev.aws-eu-west-1.turso.io,
   authToken: process.env.TURSO_AUTH_TOKEN!,
 });
