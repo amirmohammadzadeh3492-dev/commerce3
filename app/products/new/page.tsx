c1d1321b15baa9afff956d3368b232ea408657b5
amirmohammadzadeh3492-dev <amirmohammadzadeh3492@gmail.com>
2026-07-04 11:52:43 +0330

    Update page.tsx

--- app/products/new/page.tsx
+++ app/products/new/page.tsx
@@ -1,5 +1,10 @@
 import ProductForm from "@/components/products/ProductForm";
 
 export default function Page() {
-  return <ProductForm />;
+  return (
+    <div>
+      <h1>ساخت محصول</h1>
+      <ProductForm />
+    </div>
+  );
 }
