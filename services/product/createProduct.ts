607f270cf44c0872e235857d311e2bc6107b2c05
amirmohammadzadeh3492-dev <amirmohammadzadeh3492@gmail.com>
2026-07-04 11:19:17 +0330

    Create createProduct.ts

---
+++ services/product/createProduct.ts
@@ -0,0 +1,13 @@
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
+import { db } from "@/lib/firebase";
+
+export async function createProduct(data: {
+  title: string;
+  price: number;
+}) {
+  return await addDoc(collection(db, "products"), {
+    title: data.title,
+    price: data.price,
+    createdAt: serverTimestamp(),
+  });
+}
