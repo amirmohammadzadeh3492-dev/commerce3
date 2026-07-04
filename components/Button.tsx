3d2ef732581f0fe55de6fd8cfb1731219c0b87cc
amirmohammadzadeh3492-dev <amirmohammadzadeh3492@gmail.com>
2026-07-04 10:59:15 +0330

    Create Button.tsx

---
+++ components/Button.tsx
@@ -0,0 +1,22 @@
+export default function Button({
+  children,
+  onClick,
+}: {
+  children: React.ReactNode;
+  onClick?: () => void;
+}) {
+  return (
+    <button
+      onClick={onClick}
+      style={{
+        padding: "10px 15px",
+        background: "black",
+        color: "white",
+        border: "none",
+        borderRadius: 6,
+      }}
+    >
+      {children}
+    </button>
+  );
+}
