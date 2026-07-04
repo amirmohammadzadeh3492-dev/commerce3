61f0d0bace95d05e63e005f9ab546376eb959672
amirmohammadzadeh3492-dev <amirmohammadzadeh3492@gmail.com>
2026-07-04 14:16:12 +0330

    Update page.tsx

--- app/products/new/page.tsx
+++ app/products/new/page.tsx
@@ -1,11 +1,10 @@
-import ProductForm from "../../../components/products/ProductForm";
+import ProductForm from "@/components/products/ProductForm";
+
 export default function Page() {
   return (
     <div>
-      <h1>TEST PAGE</h1>
-      <button onClick={() => alert("OK")}>
-        TEST BUTTON
-      </button>
+      <h1>ثبت محصول</h1>
+      <ProductForm />
     </div>
   );
 }
