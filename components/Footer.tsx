36dc216aea2ca58b78fde2e719c25a4018b1b754
amirmohammadzadeh3492-dev <amirmohammadzadeh3492@gmail.com>
2026-07-04 10:58:53 +0330

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{ padding: 20, borderTop: "1px solid #eee", marginTop: 40 }}>
+      <p>© 2026 Commerce AI</p>
+    </footer>
+  );
+}
