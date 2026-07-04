990d2f779c41d0b540c60a4f8e97db7fb2deabcf
amirmohammadzadeh3492-dev <amirmohammadzadeh3492@gmail.com>
2026-07-04 11:53:08 +0330

    Update page.tsx

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,8 +1,27 @@
-import { getProducts } from "@/services/product/getProducts";
-import ProductList from "@/components/products/ProductList";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/lib/firebase";
+
+async function getProducts() {
+  const snap = await getDocs(collection(db, "products"));
+
+  return snap.docs.map((d) => ({
+    id: d.id,
+    ...d.data(),
+  }));
+}
 
 export default async function Page() {
   const products = await getProducts();
 
-  return <ProductList products={products} />;
+  return (
+    <div>
+      <h1>محصولات</h1>
+
+      {products.map((p: any) => (
+        <div key={p.id}>
+          {p.title} - {p.price}
+        </div>
+      ))}
+    </div>
+  );
 }
