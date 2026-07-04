f4ee080e87894c5ba42cda5e3b696cdd169b2c89
amirmohammadzadeh3492-dev <amirmohammadzadeh3492@gmail.com>
2026-07-04 13:54:30 +0330

    Update ProductForm.tsx

--- components/products/ProductForm.tsx
+++ components/products/ProductForm.tsx
@@ -1,40 +1,17 @@
-"use client";
+const handleSubmit = async () => {
+  console.log("CLICKED");
 
-import { useState } from "react";
-import { createProduct } from "@/services/product/createProduct";
-
-export default function ProductForm() {
-  const [title, setTitle] = useState("");
-  const [price, setPrice] = useState("");
-
-  const handleSubmit = async () => {
-    console.log("CLICKED");
-
-    await createProduct({
+  try {
+    const res = await createProduct({
       title,
       price: Number(price),
     });
 
-    alert("محصول ثبت شد");
-  };
+    console.log("SUCCESS:", res);
 
-  return (
-    <div>
-      <input
-        placeholder="title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-
-      <input
-        placeholder="price"
-        value={price}
-        onChange={(e) => setPrice(e.target.value)}
-      />
-
-      <button onClick={handleSubmit}>
-        ثبت محصول
-      </button>
-    </div>
-  );
-}
+    alert("OK SAVED");
+  } catch (e) {
+    console.error("FORM ERROR:", e);
+    alert("FAILED");
+  }
+};
