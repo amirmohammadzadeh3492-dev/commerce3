301e414d15816401fd082db1acebecdee2534d77
amirmohammadzadeh3492-dev <amirmohammadzadeh3492@gmail.com>
2026-07-04 11:50:58 +0330

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,12 +4,10 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyCrcO3Rit3x--P6r_hKh60WiNuUjce5BXs",
   authDomain: "commerce-platform-a1c32.firebaseapp.com",
-  databaseURL: "https://commerce-platform-a1c32-default-rtdb.firebaseio.com",
   projectId: "commerce-platform-a1c32",
   storageBucket: "commerce-platform-a1c32.firebasestorage.app",
   messagingSenderId: "185585173109",
-  appId: "1:185585173109:web:51294b119e2a4005b8ee3c",
-  measurementId: "G-KRQJF9Z27C"
+  appId:"1:185585173109:web:51294b119e2a4005b8ee3c",
 };
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
